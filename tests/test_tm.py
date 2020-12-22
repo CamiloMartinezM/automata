f203c448b572fc41e991ce756efc3bf81f1743c2
CamiloMartinezM <ca.martinezm2@uniandes.edu.co>
2020-12-22 16:24:26 -0500

    Wrong plurals

--- tests/test_tm.py
+++ tests/test_tm.py
@@ -93,7 +93,7 @@ class TestTM(object):
             final_states={'q3'}
         )
         # MNTM which accepts all strings in {0, 1}* and writes all
-        # 1's from the first tape (input) to the second tape.
+        # '1's from the first tape (input) to the second tape.
         self.mntm1 = MNTM(
             states={'q0', 'q1'},
             input_symbols={'0', '1'},
@@ -110,7 +110,7 @@ class TestTM(object):
             blank_symbol='#',
             final_states={'q1'},
         )
-        # MNTM which accepts all strings with a number of 0's of the form
+        # MNTM which accepts all strings with a number of '0's of the form
         # n^2, i.e is a square number. The string starts with '#'.
         self.mntm2 = MNTM(
             states=set(['q' + str(i)
@@ -141,14 +141,14 @@ class TestTM(object):
                     ],  # Testing whether tape 1 and 2 have the same length
                     ('0', '#', '#'): [
                         ('q3', (('0', 'N'), ('#', 'N'), ('#', 'N')))
-                    ],  # length of tape 1 is greater than tape 2'N (continues)
+                    ],  # length of tape 1 is greater than tape 2's (continues)
                     ('#', '#', '#'): [
                         ('qf', (('#', 'N'), ('#', 'N'), ('#', 'N')))
                     ],  # tape 1 and 2 were found to be of equal length
                         # accepts
                     ('#', '0', '#'): [
                         ('qr', (('#', 'N'), ('0', 'N'), ('#', 'N')))
-                    ],  # length of tape 2 is greater than tape 1'N (rejects)
+                    ],  # length of tape 2 is greater than tape 1's (rejects)
                 },
                 'q3': {
                     ('0', '#', '#'): [('q4', (('0', 'N'), ('#', 'N'),
